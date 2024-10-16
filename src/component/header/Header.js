2c632e2f86692a3d8281366c158d8c9a84e4883a
oscar.melerocamacho <oscar.melerocamacho@plexus.es>
2024-10-16 09:04:03 +0200

    Header

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -6,7 +6,10 @@ const Header = () => {
         <header style={{marginBottom:heightData}}>
             <Navbar expand="lg" className="header" fixed="top" style={{height: heightData}} bg="dark">
                 <Container fluid>
-                    <Navbar.Brand style={{color:"white"}}>Activity planner</Navbar.Brand>
+                    <Navbar.Brand href="/" style={{color:"white"}}>Activity planner</Navbar.Brand>
+                    <Nav>
+                        <Nav.Link style={{color:"white"}} href="/login">Login</Nav.Link>
+                    </Nav>
                 </Container>
             </Navbar>
         </header>
